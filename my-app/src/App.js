38d5103478400a90c4aaa376a9a253e5f6fd12fc
SakethSaimhaReddy <130767932+SakethSaimhaReddy@users.noreply.github.com>
2023-06-08 17:37:19 +0530

    commit

---
+++ my-app/src/App.js
@@ -0,0 +1,20 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="nav">
+      <header className="App-header">
+        <nav className='navitem'>
+        <h1>Placer</h1>
+        <ul className='list' >
+          <li>Home</li>
+          <li>Services</li>
+          <li>About us</li>
+          <li>Contact</li>
+          </ul>
+          </nav>
+      </header>
+    </div>
+  );
+}
+export default App;
---
+++ my-app/src/main.js
@@ -0,0 +1,20 @@
+import img1 from './img/home.jpg'
+import './main.css'
+function Main() {
+    return (
+      <div className="Main">
+        <header className="Main-header">
+         <img src={img1} alt='home'></img>
+         <div>
+         <h1>
+            Discover Endless Oppurtunities with Placer: Where Students Thrive and Alumni Guide
+         </h1>
+         </div>
+         <div>
+          
+         </div>
+        </header>
+      </div>
+    );
+  }
+  export default Main;
